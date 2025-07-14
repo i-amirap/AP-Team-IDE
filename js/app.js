4cadbc7972f8ad9004b2bd9258ad320c06d49fe1
AmirAP <104590638+i-amirap@users.noreply.github.com>
2025-07-14 20:18:11 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -300,7 +300,7 @@ window.onload = function () {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("sw.js")
+      .register("/AP-Team-IDE/sw.js")
       .then((registration) => {
         console.log("ServiceWorker ثبت شد با موفقیت: ", registration.scope);
       })
